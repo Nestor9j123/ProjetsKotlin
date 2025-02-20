e5377013235c83065cac2ec5fd3adb11ed9ce183
Nestor9j123 <kpadjanestor78@gmail.com>
2025-02-20 23:22:24 +0000

    Apprendre comment cacher des textView et les afficher etc

--- app/src/main/java/com/example/base/MainActivity.kt
+++ app/src/main/java/com/example/base/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.base
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,20 +22,26 @@ class MainActivity : AppCompatActivity() {
         val connect = findViewById<Button>(R.id.button)
         val email = findViewById<EditText>(R.id.text1)
         val password = findViewById<EditText>(R.id.text2)
+        val error = findViewById<TextView>(R.id.erreur)
 
         connect.setOnClickListener {
+            error.visibility = View.GONE
             val testEmail = email.text.toString()
             val testPassword = password.text.toString()
             if(testEmail.trim().isEmpty() || testPassword.trim().isEmpty()){
-                Toast.makeText(this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show()
+               error.text = "Veuillez remplir tous les champs"
+                error.visibility = View.VISIBLE
             }else{
 
                 val emailco= "Nestor@gmail.com"
                 val passwordco= "123456"
                 if(testEmail == emailco && testPassword == passwordco){
+
                     Toast.makeText(this, "Connexion réussie", Toast.LENGTH_SHORT).show()
-                }else
-                    Toast.makeText(this, "Email ou mot de passe incorrect", Toast.LENGTH_SHORT).show()
+                }else {
+                    error.text = "Email ou le password n'est pas coorecte "
+                    error.visibility = View.VISIBLE
+                }
             }
 
         }
